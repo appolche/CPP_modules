7d40cd6a320522f4baef3dff728c31711f635158
Danny Leaves <dleaves@at-d4.msk.21-school.ru>
2022-03-28 15:26:12 +0300

    1

---
+++ d00/ex00/megaphone.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main (void) {
+    char buff[512];
+
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return 0;
+}
+
+//if (argc > 0)
+//recieve smth from argv[i]
+//make it uppercase and print in stdout (what about special chars?)
+//else
+//print (* LOUD AND UNBEARABLE FEEDBACK NOISE *)
